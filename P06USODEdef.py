4ee34406299c880691ddf5d383af06b4e7a838f2
yosmarivangmalicom-lgtm <yosmarivangmali.com@gmail.com>
2026-04-18 17:27:29 -0600

    Add files via upload

---
+++ P06USODEdef.py
@@ -0,0 +1,7 @@
+def calcular_promedio(a, b, c):
+    promedio = (a + b + c) / 3
+    return promedio
+
+# Llamar la función
+resultado = calcular_promedio(8, 7, 9)
+print("El promedio es:", resultado)
